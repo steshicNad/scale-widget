d6dfbd5f70e69cef40396f7c6bf9ac2f35330813
nadya <nadyasteshits@yandex.ru>
2023-01-19 19:49:49 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld>
+
+  </HelloWorld>
+</template>
+
+<style>
+ @import './style.css';
+
+* {
+  box-sizing: border-box;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,10 @@
+<template></template>
+
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+});
+</script>
